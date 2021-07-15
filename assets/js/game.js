bde25f50bd8324b60a8d62f6540c160a81d43141
Achmad Syarief Hidayatullah Amiruddin <33272892+achmadsyariefha@users.noreply.github.com>
2021-07-15 23:41:42 +0800

    fourth change
    changing plan but still....

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,18 @@
-function barFunction(){
-    var x = document.getElementById("item");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
+// function barFunction(){
+//     var x = document.getElementById("item");
+//     if (x.style.display === "block") {
+//         x.style.display = "none";
+//     } else {
+//         x.style.display = "block";
+//     }
+// }
+
+const hamburger = document.querySelector(".bar-icon");
+const menu = document.querySelector("#item");
+
+hamburger.addEventListener("click", mobileView);
+
+function mobileView() {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
 }
