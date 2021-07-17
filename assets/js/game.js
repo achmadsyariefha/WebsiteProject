50f3b7ce7c818026b55b8447ee7dfff076f3f60d
Achmad Syarief Hidayatullah Amiruddin <33272892+achmadsyariefha@users.noreply.github.com>
2021-07-17 12:13:01 +0800

    fifth change
    just like an nba coach, making an adjustment. this might be our final change (sort of..)

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,7 @@
-// function barFunction(){
-//     var x = document.getElementById("item");
-//     if (x.style.display === "block") {
-//         x.style.display = "none";
-//     } else {
-//         x.style.display = "block";
-//     }
-// }
-
 const hamburger = document.querySelector(".bar-icon");
 const menu = document.querySelector("#item");
+let selectAnchor = 'a[href^="#"]';
+let listAnchor = document.querySelectorAll(selectAnchor);
 
 hamburger.addEventListener("click", mobileView);
 
@@ -16,3 +9,15 @@ function mobileView() {
     hamburger.classList.toggle("active");
     menu.classList.toggle("active");
 }
+
+listAnchor.forEach(link => {
+    link.onclick = function (e) {
+        e.preventDefault();
+
+        let destination = document.querySelector(this.hash);
+
+        destination.scrollIntoView({
+            'behavior': 'smooth'
+        });
+    }
+})
